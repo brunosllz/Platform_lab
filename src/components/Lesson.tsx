69df67be3f24851f62cba90d96411736ea6f5b56
Bruno Luiz <brunoslvrlz13@gmail.com>
2022-06-23 01:35:59 -0300

    Implement video component and router navigaton

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from 'phosphor-react';
 import { format, isPast } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR'
+import { Link } from 'react-router-dom';
 
 
 
@@ -18,12 +19,12 @@ export function Lesson(props: LessonProps) {
   })
 
   return (
-    <a href="#">
+    <Link to={`/event/lesson/${props.slug}`} className='group'>
       <span className="text-gray-300 capitalize">
         {availableAtFormatted}
       </span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2">
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 transition-colors">
         <header className="flex items-center justify-between">
           {
             isLessonAvailable ? (
@@ -48,6 +49,6 @@ export function Lesson(props: LessonProps) {
           {props.title}
         </strong>
       </div>
-    </a>
+    </Link>
   )
 }
