02101b4ab23bb04bb800a87c9407a0b7fc5ad953
Bruno Luiz <brunoslvrlz13@gmail.com>
2022-06-26 19:42:03 -0300

    Remove event page at routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,12 @@
 import { Route, Routes, } from "react-router-dom";
 import { Subscriber } from "./pages/Subscriber";
-import { Event } from "./pages/Event";
+
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Subscriber />} />
-      <Route path="/event" element={<Event />} />
-      <Route path="/event/lesson/:slug" element={<Event />} />
+
     </Routes >
   )
 }
